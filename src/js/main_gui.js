e5d6078ef2fb4a46d29dddbea1f2c9f4324a23e4
xBZZZZ <alanas.00@mail.ru>
2023-11-02 12:31:22 +0200

    ios gd uses same save format as mac os gd https://gdforum.freeforums.net/post/921979/thread

--- src/js/main_gui.js
+++ src/js/main_gui.js
@@ -16,10 +16,10 @@ don&apos;t read entire file because there can be 1 or 2 bytes of garbage because
 </ol>\
 </details>\
 <details open="" class="bigd">\
-<summary>how to decode CC*.dat file (mac os)?</summary>\
+<summary>how to decode CC*.dat file (mac os and ios)?</summary>\
 <ol>\
 <li><strong><a href="https://www.highgo.ca/2019/08/08/the-difference-in-five-modes-in-the-aes-encryption-algorithm/" rel="noreferrer" target="_blank">AES-ECB</a> 256</strong> decrypt with key=<wbr/><q class="wpb" style="quotes:none;" cite="https://github.com/Wyliemaster/GD-Save-Decryptor/blob/main/saves.py#L43">ipu9TUv54yv]isFMh5@;t.5w34E2Ry@{</q></li>\
-<li>remove padding (last <var style="font-weight:bold;">value of last byte</var> bytes are equal and &lt;16 and are padding)</li>\
+<li>remove padding (last <var style="font-weight:bold;">value of last byte</var> bytes are equal and &#8804; 16 and are padding)</li>\
 </ol></details>\
 <details open="" class="bigd">\
 <summary>geometry dash can load non-encoded XML files (windows and android)</summary><ul>\
@@ -31,16 +31,16 @@ don&apos;t read entire file because there can be 1 or 2 bytes of garbage because
 <code style="white-space:pre-wrap;color:#000;"><span style="color: #0099FF; font-style: italic"># replace $CCFILE with input file path&#10;# replace $DECODEDCCFILE with output file path</span>&#10;xxd -p -c4 <span style="color: #CC3300">&quot;$CCFILE&quot;</span> | gawk <span style="color: #CC3300">&#39;length($0)==8{printf&quot;%08x&quot;,xor(0xb0b0b0b,strtonum(&quot;0x&quot;$0))}&#39;</span> | xxd -p -r | tr <span style="color: #CC3300">&#39;_-&#39;</span> <span style="color: #CC3300">&#39;/+&#39;</span> | base64 -d | zcat &gt; <span style="color: #CC3300">&quot;$DECODEDCCFILE&quot;</span></code>\
 </details>\
 <details style="background-color:#f0f3f3;" open="" class="bigd">\
-<summary>mac os and linux shell command to decode CC*.dat file (mac os)</summary>\
+<summary>mac os and linux shell command to decode CC*.dat file (mac os and ios)</summary>\
 <code style="white-space:pre-wrap;color:#000;"><span style="color: #0099FF; font-style: italic"># replace $CCFILE with input file path&#10;# replace $DECODEDCCFILE with output file path</span>&#10;openssl aes-256-ecb -in <span style="color: #CC3300">&quot;$CCFILE&quot;</span> -out <span style="color: #CC3300">&quot;$DECODEDCCFILE&quot;</span> -d -K 69707539545576353479765D6973464D6835403B742E3577333445325279407B</code>\
 </details>\
 <details open="" class="bigd">\
-<summary>other CC*.dat file tools</summary><strong>WARNING (doesn&apos;t apply to mac os saves):</strong> some of these sometimes don&apos;t work because they read entire file instead of just <strong class="wp">floor(<var>file size in bytes</var> / 4) * 4</strong> bytes<ul>\
+<summary>other CC*.dat file tools</summary><strong>WARNING (doesn&apos;t apply to mac os and ios saves):</strong> some of these sometimes don&apos;t work because they read entire file instead of just <strong class="wp">floor(<var>file size in bytes</var> / 4) * 4</strong> bytes<ul>\
 <li>'+linkf('https://www.youtube.com/watch?v=OA4918DQxG0')+'</li>\
 <li>'+linkf('https://gdcolon.com/gdsave/')+'</li>\
 <li>'+linkf('https://github.com/WEGFan/Geometry-Dash-Savefile-Editor')+'</li>\
 <li>'+linkf('https://github.com/Wyliemaster/GD-Save-Tools')+'</li>\
-<li>'+linkf('https://github.com/Wyliemaster/GD-Save-Decryptor')+' (can decrypt mac os saves, doesn&apos;t remove padding when padding=16)</li>\
+<li>'+linkf('https://github.com/Wyliemaster/GD-Save-Decryptor')+' (can decrypt mac os and ios saves, doesn&apos;t remove padding when padding=16)</li>\
 <li>'+linkf('https://github.com/GDColon/GD-Save-Decoder')+'</li>\
 <li>'+linkf('https://geometry-dash.fandom.com/wiki/User:XBZZZZALT#backup_of_useful_stuff_from_Save_Files_page')+'</li>\
 <li>'+linkf('https://wyliemaster.github.io/gddocs/#/topics/localfiles_encrypt_decrypt')+'</li>\
@@ -56,7 +56,7 @@ push_gui(gui_div_with_html(false,
 <h2>make a backup of CCGameManager.dat and CCLocalLevels.dat to be able to unbreak geometry dash</h2>\
 <input class="thiccb" type="file"/>\
 <input class="thiccb" type="button" value="load encrypted (windows and android)" '+(window.DecompressionStream?'onclick="var f=current_gui().drop_file_input.files[0];if(f)cc_load_gzip(f);else window.alert(&quot;no file&quot;);"':'title="your browser doesn&apos;t support DecompressionStream" disabled=""')+'/>\
-<input class="thiccb" type="button" value="load encrypted (mac os)" '+(typeof subtlecrypto==='object'?'onclick="var f=current_gui().drop_file_input.files[0];if(f)cc_load_aes(f);else window.alert(&quot;no file&quot;);"':subtlecrypto)+'/>\
+<input class="thiccb" type="button" value="load encrypted (mac os and ios)" '+(typeof subtlecrypto==='object'?'onclick="var f=current_gui().drop_file_input.files[0];if(f)cc_load_aes(f);else window.alert(&quot;no file&quot;);"':subtlecrypto)+'/>\
 <input class="thiccb" type="button" value="load xml" onclick="var f=current_gui().drop_file_input.files[0];if(f)cc_load_xml(f);else window.alert(&quot;no file&quot;);"/>\
 <hr/>\
 <input class="thiccb" type="button" value="open encoded string editor" onclick="string_editor(null,&quot;&quot;);"/>\
